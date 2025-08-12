52b3f40d03b96189a056e077c5918c2fb55dd090
BFBacchi <bfbacchi@gmail.com>
2025-08-12 20:50:48 -0300

    fix app.js

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
-// ...el resto del código permanece igual...
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+
+dotenv.config();
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Conexión a la base de datos
+connectDB();
+
+// Rutas de productos
+app.use('/api/products', productRoutes);
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // Exportar como función serverless para Vercel
+const serverless = require('serverless-http');
+module.exports = serverless(app);
