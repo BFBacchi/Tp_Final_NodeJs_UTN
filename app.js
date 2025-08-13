02a22692a58f070bbf2c4d5ca93a903c748f9624
BFBacchi <bfbacchi@gmail.com>
2025-08-12 22:07:58 -0300

    se elimina cors

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 
 const express = require('express');
-const path = require('path');
-const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./docs/swagger.yaml');
+
 
 dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
 app.use(express.json());
 
 // Conexión a la base de datos
@@ -22,8 +17,6 @@ connectDB();
 // Rutas de productos
 app.use('/api/products', productRoutes);
 
-// Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Exportar como función serverless para Vercel
 //const serverless = require('serverless-http');
